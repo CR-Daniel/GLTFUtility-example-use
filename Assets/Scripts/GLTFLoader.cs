2c983ad61175c4236b555ff88ac2e09d1691a452
CR-Daniel <d.chou.rainho@student.tue.nl>
2023-07-19 12:00:30 +0200

    progress bar

--- Assets/Scripts/GLTFLoader.cs
+++ Assets/Scripts/GLTFLoader.cs
@@ -1,18 +1,20 @@
 using Siccity.GLTFUtility;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GLTFLoader : MonoBehaviour
 {
-    public string relativeFilePath = "tree01.gltf";
+    [SerializeField] private Slider progressBar;
+    private string relativeFilePath = "tree01.gltf";
 
-    void Start()
+    private void Start()
     {
         string absoluteFilePath = System.IO.Path.Combine(Application.streamingAssetsPath, relativeFilePath);
 
-        Importer.LoadFromFileAsync(absoluteFilePath, new ImportSettings(), OnFinishAsync);
+        Importer.LoadFromFileAsync(absoluteFilePath, new ImportSettings(), OnFinishAsync, OnProgress);
     }
 
-    void OnFinishAsync(GameObject result, AnimationClip[] animations)
+    private void OnFinishAsync(GameObject result, AnimationClip[] animations)
     {
         if(result != null)
         {
@@ -22,5 +24,10 @@ public class GLTFLoader : MonoBehaviour
         {
             Debug.Log("Failed to load the glTF file.");
         }
+    }
+
+    private void OnProgress(float progress)
+    {
+        progressBar.value = progress;
     }
 }
